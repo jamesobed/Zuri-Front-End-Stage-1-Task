eb9b936af10dcce05ffc470377b7f8b7de378082
jamesobed <jamesobed4@gmail.com>
2022-11-05 03:01:18 +0100

    fixed routed wild route issue

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import "./App.css";
+// import "./App.css";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
 
 import * as React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
